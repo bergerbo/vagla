efb35d74afb5ffa5f61d2fdea2e2086d8cf95a82
Berger Boris <berger.boris@orange.fr>
2014-11-26 18:57:13 +0100

    Ajout d'une creation d'arbre par defaut, affichage d'arbre et debut de comptage.

--- briandais.py
+++ briandais.py
@@ -52,3 +52,42 @@ class Node():
 
         else :
             self.sibling.add_word(word)
+
+    def print_tree(self, prefix):
+        if self.value == end_of_word :
+            print repr(prefix)
+            #print "\n"
+        elif self.child is not None :
+            self.child.print_tree(prefix + self.value)
+        if self.sibling is not None :
+            self.sibling.print_tree(prefix)
+    
+    def count_words(self, cpt) :
+        ncpt = cpt
+        if self.value == end_of_word :
+            ncpt += 1
+        elif self.child is not None :
+            ncpt += self.child.count_words(cpt)
+        if self.sibling is not None :
+            return self.sibling.count_words(ncpt)
+        else :
+            return ncpt
+
+
+def Recherche(node, word):
+    return node.has_word(word)
+
+def ComptageMots(node):
+    return node.count(0)
+
+def ExampleBase():
+    example_base = """A quel genial professeur de dactylographie sommes nous
+    redevables de la superbe phrase ci dessous, un
+    modele du genre, que toute dactylo connait par coeur puisque elle fait appel
+    a chacune des touches du
+    clavier de la machine a ecrire?"""
+    list = example_base.split(" ")
+    node = build_nodes(list[0])
+    for word in list[1:] :
+        node.add_word(word)
+    return node
